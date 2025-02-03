6e9bcd4de207ec64fcec800f44b41702da6ec0da
julien-35 <julien45.dubois@gmail.com>
2025-02-03 16:59:52 +0100

    Intallation des dépendances et de la première entité et lien à mysql

--- src/main/java/com/example/application/entities/Client.java
+++ src/main/java/com/example/application/entities/Client.java
@@ -4,13 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 
 @Entity  // Indique que c'est une entité JPA
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // Génère un identifiant unique
     private Long id;
-    private String nom;
+    private String numeroClient; 
+    private String denomination;
+    private String adresse;
+    private String codePostal;
+    private String ville;
+    private String secteurActivite;
+    private String siret;
+    private String dateCreation;
 
     // Getters et Setters
     public Long getId() {
@@ -21,11 +29,67 @@ public class Client {
         this.id = id;
     }
 
-    public String getNom() {
-        return nom;
+    public String getNumeroClient() {
+        return numeroClient;
     }
 
-    public void setNom(String nom) {
-        this.nom = nom;
+    public void setNumeroClient(String numeroClient) {
+        this.numeroClient = numeroClient;
+    }
+
+    public String getDenomination() {
+        return denomination;
+    }
+
+    public void setDenomination(String denomination) {
+        this.denomination = denomination;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getSecteurActivite() {
+        return secteurActivite;
+    }
+
+    public void setSecteurActivite(String secteurActivite) { 
+        this.secteurActivite = secteurActivite; 
+    }
+
+    public String getSiret() {
+        return siret;
+    }
+
+    public void setSiret(String siret) {
+        this.siret = siret;
+    }
+
+    public String getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(String dateCreation) {
+        this.dateCreation = dateCreation;
     }
 }
